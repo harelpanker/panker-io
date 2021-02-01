d02cadb526cb3ed0a840d064acc574966c8160eb
Harel Panker <harelpanker@gmail.com>
2021-02-01 14:44:55 +0100

    Add validify project

--- public/projects.js
+++ public/projects.js
@@ -3,87 +3,94 @@ import uuid from 'react-uuid';
 const projectsList = [
   {
     id: uuid(),
-    name: 'Women \'N\' Fitness',
+    name: "Women 'N' Fitness",
     img: '/images-projects/img8.png',
     liveURL: 'https://womennfitness.com/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
-    name: 'Modern Family',
-    img: '/images-projects/img9.png',
-    liveURL: 'https://miri-blog.webflow.io/',
-    type: 'webflow'
+    name: 'Validify.app',
+    img: '/images-projects/validify.png',
+    liveURL: 'https://www.validify.app/',
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'Parents Home',
     img: '/images-projects/img1.jpg',
     liveURL: 'https://www.parentshome.co.il/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'Take A Seat',
     img: '/images-projects/img2.jpg',
     liveURL: 'https://www.takeaseat.at/',
-    type: 'webflow'
+    type: 'webflow',
+  },
+  {
+    id: uuid(),
+    name: 'Modern Family',
+    img: '/images-projects/img9.png',
+    liveURL: 'https://miri-blog.webflow.io/',
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'Constructions',
     img: '/images-projects/img3.jpg',
     liveURL: 'https://www.oferazulay.co.il/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'Video Creative',
     img: '/images-projects/img4.jpg',
     liveURL: 'https://www.baigel.co/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'My Community',
     img: '/images-projects/img5.jpg',
     liveURL: 'https://my-comunity.webflow.io/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'La Casa Bar-Caffee',
     img: '/images-projects/img6.jpg',
     liveURL: 'https://lacasa.webflow.io/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'Alcohol E-commerce',
     img: '/images-projects/img7.jpg',
     liveURL: 'https://whisky-lovers.webflow.io/',
-    type: 'webflow'
+    type: 'webflow',
   },
   {
     id: uuid(),
     name: 'unSplash',
     img: '/images-projects/unsplash.png',
     liveURL: 'https://unsplash-panker.vercel.app/',
-    type: 'react'
+    type: 'react',
   },
   {
     id: uuid(),
     name: 'Weather',
     img: '/images-projects/weather.png',
     liveURL: 'https://weather2021.vercel.app/',
-    type: 'react'
+    type: 'react',
   },
   {
     id: uuid(),
     name: 'Breaking Bad',
     img: '/images-projects/breaking.png',
     liveURL: 'https://breaking-bad-six.vercel.app/',
-    type: 'react'
+    type: 'react',
   },
 ];
 export default projectsList;
