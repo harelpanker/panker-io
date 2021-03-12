ff1fc00b1def32ea25116efe9ef254ad9768d684
harelpanker <harelpanker@gmail.com>
2021-03-12 10:52:41 +0100

    skills

--- public/skills.js
+++ public/skills.js
@@ -1,18 +1,17 @@
-import uuid from 'react-uuid'
+import uuid from 'react-uuid';
 
 const skills = [
-    { id: uuid(), name: 'HTML' },
-    { id: uuid(), name: 'CSS / SCSS' },
-    { id: uuid(), name: 'Bootstrap' },
-    { id: uuid(), name: 'Material-UI and' },
-    { id: uuid(), name: 'Styled-Components' },
-    { id: uuid(), name: 'JavaScript' },
-    { id: uuid(), name: 'React.js, NextJS' },
-    { id: uuid(), name: 'Figma' },
-    { id: uuid(), name: 'Wordpress' },
-    { id: uuid(), name: 'Webflow' },
-    { id: uuid(), name: 'SEO' },
-    { id: uuid(), name: 'UI/UX' },
-]
+  { id: uuid(), name: 'HTML' },
+  { id: uuid(), name: 'CSS / SCSS' },
+  { id: uuid(), name: 'JavaScript' },
+  { id: uuid(), name: 'React.js, NextJS' },
+  { id: uuid(), name: 'Material-UI' },
+  { id: uuid(), name: 'Styled-Components' },
+  { id: uuid(), name: 'Google' },
+  { id: uuid(), name: 'SEO' },
+  { id: uuid(), name: 'UI/UX' },
+  { id: uuid(), name: 'Figma' },
+  { id: uuid(), name: 'Webflow' },
+];
 
-export default skills
+export default skills;
